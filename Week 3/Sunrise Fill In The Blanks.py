7b31b55fb1ceebeb1fccc36c99be2d626eea129e
mirzayasirabdullahbaig07 <yasirabdullah4549@gmail.com>
2026-05-20 13:34:58 +0500

    Sunrise Fill in the Blanks

---
+++ Week 3/Sunrise Fill In The Blanks.py
@@ -0,0 +1,11 @@
+def main():
+    # Get inputs from user
+    color = input("A color: ")
+    adjective = input("An adjective: ")
+    goal = input("A goal you would like to achieve: ")
+    
+    # Print the story
+    print(f"At dawn the sky turned {color}, and the air felt {adjective}. I decided today I will finally {goal}.")
+
+if __name__ == "__main__":
+    main()

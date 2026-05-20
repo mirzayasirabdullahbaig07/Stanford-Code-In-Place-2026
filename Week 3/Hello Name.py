0461dfef3160d1f5713d927d8b07adb282eaf05f
mirzayasirabdullahbaig07 <yasirabdullah4549@gmail.com>
2026-05-20 13:33:48 +0500

    Hello Name

---
+++ Week 3/Hello Name.py
@@ -0,0 +1,9 @@
+def main():
+    # Ask the user for their name
+    name = input("What is your name? ")
+    
+    # Print greeting
+    print("Hello", name)
+
+if __name__ == '__main__':
+    main()
